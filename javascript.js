3f793dfadd51dc534f5de30161d821827d908863
Venkat Sai <venky00584@gmail.com>
2026-09-22 23:14:11 +0530

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,248 @@
+var name = "Venkat Sai";
+console.log(name);
+
+// 2
+var age = 22;
+console.log(age);
+
+// 3
+var city;
+city = "Nellore";
+console.log(city);
+
+// 4
+var number = 20;
+number = 40;
+console.log(number);
+
+// 5
+var course = "Python";
+var course = "JavaScript";
+console.log(course);
+
+// 6
+var myName = "Venkat Sai";
+var myAge = 22;
+var myCity = "Nellore";
+console.log(myName);
+console.log(myAge);
+console.log(myCity);
+
+// 7
+var collegeName = "SRM University";
+console.log(collegeName);
+
+// 8
+var favoriteSubject = "Computer Science";
+console.log(favoriteSubject);
+
+// 9
+var marks = 10;
+marks = 20;
+marks = 30;
+marks = 40;
+console.log(marks);
+
+// 10
+var salary = 20000;
+salary = 30000;
+console.log(salary);
+
+
+// Variables - let
+
+// 11
+let studentName = "Venkat Sai";
+console.log(studentName);
+
+// 12
+let studentAge = 22;
+console.log(studentAge);
+
+// 13
+let location;
+location = "Andhra Pradesh";
+console.log(location);
+
+// 14
+let amount = 100;
+amount = 200;
+console.log(amount);
+
+// 15
+let name2 = "Venkat Sai";
+let qualification = "B.Tech CSE";
+let location2 = "Nellore";
+console.log(name2);
+console.log(qualification);
+console.log(location2);
+
+// 16
+let course2 = "JavaScript";
+console.log(course2);
+
+// 17
+let marks2 = 50;
+marks2 = 80;
+console.log(marks2);
+
+// 18
+let companyName = "Stackly";
+console.log(companyName);
+
+// 19
+let experience = 0;
+console.log(experience);
+
+// 20
+let mobileModel = "Samsung";
+console.log(mobileModel);
+
+
+// Variables - const
+
+// 21
+const constName = "Venkat Sai";
+console.log(constName);
+
+// 22
+const dateOfBirth = "2004-01-01";
+console.log(dateOfBirth);
+
+// 23
+const company = "TCS";
+console.log(company);
+
+// 24
+const country = "India";
+console.log(country);
+
+// 25
+const name3 = "Venkat Sai";
+const age3 = 22;
+const city3 = "Nellore";
+console.log(name3);
+console.log(age3);
+console.log(city3);
+
+// 26
+const courseName = "JavaScript";
+console.log(courseName);
+
+// 27
+const collegeName2 = "SRM University";
+console.log(collegeName2);
+
+// 28
+const favoriteColor = "Blue";
+console.log(favoriteColor);
+
+// 29
+const employeeId = 101;
+console.log(employeeId);
+
+// 30
+const officeLocation = "Bengaluru";
+console.log(officeLocation);
+
+
+// Printing Statements
+
+// 31
+console.log(100);
+
+// 32
+console.log("Venkat Sai");
+
+// 33
+console.log(22);
+console.log("B.Tech CSE");
+
+// 34
+console.log("Welcome to JavaScript");
+
+// 35
+console.log("Venkat Sai");
+console.log(22);
+console.log("Nellore");
+console.log("B.Tech CSE");
+console.log("JavaScript");
+
+// 36
+var userName = "Venkat Sai";
+console.log(userName);
+
+// 37
+var userAge = 22;
+console.log(userAge);
+
+// 38
+console.log("Venkat Sai");
+console.log(22);
+console.log("Nellore");
+console.log("B.Tech CSE");
+
+
+// Popup Methods
+
+// 39
+alert("Welcome to JavaScript");
+
+// 40
+var alertName = "Venkat Sai";
+alert(alertName);
+
+// 41
+var enteredName = prompt("What is your name?");
+alert(enteredName);
+
+// 42
+var enteredAge = prompt("What is your age?");
+console.log(enteredAge);
+
+// 43
+var enteredQualification = prompt("What is your qualification?");
+alert(enteredQualification);
+
+// 44
+var enteredCity = prompt("What is your city?");
+console.log(enteredCity);
+
+// 45
+confirm("Do you know JavaScript?");
+
+// 46
+confirm("Do you want to continue?");
+
+// 47
+var userName2 = prompt("What is your name?");
+var userQualification2 = prompt("What is your qualification?");
+
+document.writeln(userName2);
+document.writeln(userQualification2);
+
+
+// document.writeln() + Console Methods
+
+// 48
+var message = "Hello Everyone";
+document.writeln(message);
+
+// 49
+console.log("Venkat Sai");
+console.warn("B.Tech CSE");
+console.error("Nellore");
+
+// 50
+var userName3 = prompt("Enter your name:");
+var userAge3 = prompt("Enter your age:");
+var userQualification3 = prompt("Enter your qualification:");
+
+console.log(userName3);
+console.warn(userAge3);
+console.error(userQualification3);
+
+document.writeln("Name: " + userName3);
+document.writeln("<br>Age: " + userAge3);
+document.writeln("<br>Qualification: " + userQualification3);
+
